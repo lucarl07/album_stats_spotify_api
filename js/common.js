50266c48bb3be980db52dac210f6d1c6113f1802
lucarl07 <junitinho2007@gmail.com>
2024-03-01 08:11:09 -0300

    Add client keys + Lipsum text on index.html

--- js/common.js
+++ js/common.js
@@ -1,14 +1,14 @@
 /* FILE DESIGNED FOR COMMON USE OF THE FOLLOWING FUNCTIONS: */
 
 // Method to obtain the Spotify Web API access token:
-const clientId = ''; 
-const clientSecret = '';
+const clientId = '676cb524e96d48749f7c8bf60f64be2b'; 
+const clientSecret = 'ca2a68808edf4648aca8ad611bec7b77';
 
 export async function getToken() {
   const response = await fetch('https://accounts.spotify.com/api/token', {
     method: 'POST',
     body: new URLSearchParams({
-        'grant_type': 'client_credentials'
+      'grant_type': 'client_credentials'
     }),
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
