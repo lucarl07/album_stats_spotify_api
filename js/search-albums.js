183dc4456260cd33854a97b03bb2b9ef1b5fb2f4
lucarl07 <junitinho2007@gmail.com>
2024-03-04 10:11:30 -0300

    Fetch the "Get Album" endpoint

--- js/search-albums.js
+++ js/search-albums.js
@@ -19,14 +19,9 @@ btnRequest.addEventListener('click', () => {
 
         // Declare the offset and encoded album name for the search
         const albumName = encodeURIComponent(inGetAlbum.value);
-        let offset = 0;
-        
-        if (objectPage > 0) {
-            offset = (objectPage * 8) + 1;
-        }
 
         // Set the fetch URL with all the parameters
-        const compositeUrl = `${baseUrl}?q=${albumName}&type=album&offset=${offset}`;
+        const compositeUrl = `${baseUrl}?q=${albumName}&type=album`;
 
         // Fetch the composite URL
         const response = await fetch(compositeUrl, {
@@ -44,55 +39,76 @@ btnRequest.addEventListener('click', () => {
         }
     }
 
+    // Get more data about a specific album
+    const getAlbum = async(access_token, album_id) => {
+
+        const baseUrl = `https://api.spotify.com/v1/albums/${album_id}`;
+
+        const response = await fetch(baseUrl, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + access_token
+            }
+        })
+
+        return await response.json(); 
+    }
+
     // Call all the functions
     getToken().then(response => {
-        getArrayOfAlbums(response.access_token).then(response => {
-            showAlbums(response.results)
+        let token = response.access_token
+
+        const divResults = document.querySelector('.results');
+        getArrayOfAlbums(token).then(array => {
+            array.results.forEach((album) => {
+                // getAlbum(token, album.id).then(a => {
+                //     return a;
+                // })
+
+                // showAlbums(loool);
+            });
         })
     });
 })
 
-function showAlbums(albums) {
+function showAlbums(album) {
     const divResults = document.querySelector('.results');
-    console.log('Álbuns: \n', albums)
+    console.log('Álbum: \n', album)
     
     divResults.innerHTML = `<h2 class="area-title">Resultados:</h2>`
+    const divAlbum = document.createElement('div')
 
-    albums.map(album => {
-        const divAlbum = document.createElement('div')
+    divAlbum.id = `${album.id}`
+    divAlbum.innerHTML += `
+        <img src="${album.images[1].url}" alt="Capa do ${album.type} ${album.name}">
 
-        divAlbum.id = `${album.id}`
-        divAlbum.innerHTML = `
-            <img src="${album.images[1].url}" alt="Capa do ${album.type} ${album.name}">
+        <article class="info-box">
+            <div class="top-section">
+                <h1 class="album-title">
+                    <a href="#">${album.name}</a>
+                </h1>
 
-            <article class="info-box">
-                <div class="top-section">
-                    <h1 class="album-title">
-                        <a href="#">${album.name}</a>
-                    </h1>
-
-                    <h2 class="artist-name">
-                        <a href="${album.artists[0].external_urls.spotify}" target="_blank" rel="noopener noreferrer">
-                            ${album.artists[0].name}
-                        </a>
-                    </h2>
-
-                    <div class="other-info">
-                        <span>${getAlbumYear(album.release_date)}</span> • <span>${album.total_tracks} ${trackOrTracks(album.total_tracks)}</span>
-                    </div>
-                </div>
-
-                <div class="bottom-section">
-                    <a  class="spotify-url" href="${album.external_urls.spotify}" target="_blank" rel="noopener noreferrer">
-                        Ver álbum no Spotify
+                <h2 class="artist-name">
+                    <a href="${album.artists[0].external_urls.spotify}" target="_blank" rel="noopener noreferrer">
+                        ${album.artists[0].name}
                     </a>
-                </div>
-            </article>
-        `;
+                </h2>
 
-        divAlbum.classList.add('album')
-        divResults.appendChild(divAlbum)
-    })
+                <div class="other-info">
+                    <span>${getAlbumYear(album.release_date)}</span> • <span>${album.total_tracks} ${trackOrTracks(album.total_tracks)}</span>
+                </div>
+            </div>
+
+            <div class="bottom-section">
+                <a  class="spotify-url" href="${album.external_urls.spotify}" target="_blank" rel="noopener noreferrer">
+                    Ver álbum no Spotify
+                </a>
+            </div>
+        </article>
+    `;
+
+    divAlbum.classList.add('album')
+    divResults.appendChild(divAlbum)
 }
 
 function getAlbumYear(date) {
